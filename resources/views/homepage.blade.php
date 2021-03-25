ff9dcc4afa1463229eaee3e1eeb84741e9437136
Michael Daly <hello@michaeldaly.me>
2021-03-25 19:01:46 +0000

    optimise the appspec and buildspec files

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 
-<h1>Hello - Version 2</h1>
+<h1>Hello - Version 3 - Dynamic ENV</h1>
 
 @endsection
--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
-# Set permissions to storage and bootstrap cache
-sudo chmod -R 0777 /var/www/md-test-project/storage
-sudo chmod -R 0777 /var/www/md-test-project/bootstrap/cache
 
 #
 cd /var/www/md-test-project
+
+
+#
+# Copy the environment file from S3 to the local installation
+aws s3 cp s3://md-test-project-builds/production.env .env
+
+
+# Set permissions to storage and bootstrap cache
+# sudo chmod -R 0777 /var/www/md-test-project/storage
+# sudo chmod -R 0777 /var/www/md-test-project/bootstrap/cache
+sudo chmod -R 0777 storage
+sudo chmod -R 0777 bootstrap/cache
+
 
 #
 # Run composer
 # sudo /usr/bin/composer.phar install --no-ansi --no-dev --no-suggest --no-interaction --no-progress --prefer-dist --no-scripts -d /var/www/md-test-project
 
+
 # Clear any previous cached views and optimize the application
-php /var/www/md-test-project/artisan cache:clear
-php /var/www/md-test-project/artisan view:clear
-php /var/www/md-test-project/artisan config:cache
-php /var/www/md-test-project/artisan optimize
-php /var/www/md-test-project/artisan route:cache
+php artisan cache:clear
+php artisan view:clear
+php artisan config:cache
+php artisan optimize
+php artisan route:cache
+
 
 #
 # Run artisan commands
 # php /var/www/md-test-project/artisan migrate
+
 
 #
 # Start the application
