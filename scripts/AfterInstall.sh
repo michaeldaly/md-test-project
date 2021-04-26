6c64f8deb0c6e267b349c0428948169906406ce2
Michael Daly <hello@michaeldaly.me>
2021-04-26 16:40:38 +0100

    playing with deployment groups

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -2,23 +2,27 @@
 
 
 #
-cd /var/www/md-test-project
+#cd /var/www/md-test-project
 
 
 #
 # Copy the environment file from S3 to the local installation
-if [[ "$DEPLOYMENT_GROUP_NAME" == "staging-deployment-group" ]]
+if [[ "$DEPLOYMENT_GROUP_NAME" == "dev-deployment-group" ]]
 then
+    cd /var/www/md-test-project
+    aws s3 cp s3://md-test-project-builds/config-files/dev.env .env
+elif [[ "$DEPLOYMENT_GROUP_NAME" == "staging-deployment-group" ]]
+then
+    cd /var/www/md-test-project-staging
     aws s3 cp s3://md-test-project-builds/config-files/staging.env .env
 elif [[ "$DEPLOYMENT_GROUP_NAME" == "production-deployment-group" ]]
 then
+    cd /var/www/md-test-project-prod
     aws s3 cp s3://md-test-project-builds/config-files/production.env .env
 fi
 
 
 # Set permissions to storage and bootstrap cache
-# sudo chmod -R 0777 /var/www/md-test-project/storage
-# sudo chmod -R 0777 /var/www/md-test-project/bootstrap/cache
 sudo chmod -R 0777 storage
 sudo chmod -R 0777 bootstrap/cache
 
--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-cd /var/www/md-test-project
+# cd /var/www/md-test-project
+
+if [[ "$DEPLOYMENT_GROUP_NAME" == "dev-deployment-group" ]]
+then
+    cd /var/www/md-test-project
+elif [[ "$DEPLOYMENT_GROUP_NAME" == "staging-deployment-group" ]]
+then
+    cd /var/www/md-test-project-staging
+elif [[ "$DEPLOYMENT_GROUP_NAME" == "production-deployment-group" ]]
+then
+    cd /var/www/md-test-project-prod
+fi
 
 #
 # Stop the application
