85ae20beddd82d7320192110f915f41f5177a201
Michael Daly <hello@michaeldaly.me>
2021-06-18 13:00:14 +0100

    fix github action

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
